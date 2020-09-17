210385af7eac94108a717db7275a8acda0520e2f
Oksana <dzvinka220@gmail.com>
2020-09-18 00:12:27 +0300

    Updates

---
+++ precache-manifest.4febd28b7f8564f3899ea78e00240d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c636b5ea444cac7f6a5f6a350f624220",
+    "url": "/github.io/react_1/index.html"
+  },
+  {
+    "revision": "f1e35569de8dccf0e6d2",
+    "url": "/github.io/react_1/static/css/main.8601e010.chunk.css"
+  },
+  {
+    "revision": "9118fb2a13b39277b2be",
+    "url": "/github.io/react_1/static/js/2.aa5044b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react_1/static/js/2.aa5044b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e35569de8dccf0e6d2",
+    "url": "/github.io/react_1/static/js/main.84b1c2d5.chunk.js"
+  },
+  {
+    "revision": "df0516c485b4309d10a9",
+    "url": "/github.io/react_1/static/js/runtime-main.a849b80f.js"
+  }
+]);
